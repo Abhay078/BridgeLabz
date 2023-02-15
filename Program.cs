82d05362c551477d8dbc097bbdf442c55af8b277
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-15 12:56:12 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program2
+{
+
+    // Main Method
+    public static void Main()
+    {
+        Console.WriteLine("Enter a place name you like to visit most ");
+        string str1 = Console.ReadLine();
+
+        string lowerstr1 = str1.ToLower();
+        string Upperstr2 = str1.ToUpper();
+
+        Console.WriteLine("LowerCase :- "+lowerstr1);
+        Console.WriteLine("UpperCase :- "+Upperstr2);
+    }
+}
+
