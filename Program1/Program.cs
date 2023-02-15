c80eec0bce26e475e3c5cf8320ceb446ad7521bb
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-15 12:43:59 +0530

    Fisrt Commit

---
+++ Program1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program1 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alok Kumar");
+            Console.WriteLine("Prakhar Dwivedi");
+            Console.WriteLine("Nitik Sinha");
+            Console.WriteLine("Sachin Kumar yadav");
+            Console.WriteLine("Sanjay Verma");
+        }
+    }
+}
