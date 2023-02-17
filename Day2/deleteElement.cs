45b07d075327d0a99d2db8c868da1654a1cfd4d1
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-17 13:56:28 +0530

    [Abhay]-Delete_Element_array

---
+++ Day2/deleteElement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class deleteElement
+    {
+        public deleteElement()
+        {
+            Console.WriteLine("Enter number of elements you want in array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.Write("Enter elements in array :- ");
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.WriteLine("Which position element you want to delete");
+            int p = Convert.ToInt32(Console.ReadLine());
+            for (int i = p - 1; i < n-1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            for (int i = 0; i < n-1; i++)
+            {
+                Console.Write(arr[i] + " ");
+
+            }
+        }
+    }
+}
