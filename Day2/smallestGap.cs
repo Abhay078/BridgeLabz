94941adcf9b5ce4e673ec6ac2a13981fac86fab1
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-17 20:02:42 +0530

    [Abhay]-Smallest_Gap_between_Integer

---
+++ Day2/smallestGap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class smallestGap
+    {
+        int diff=int.MaxValue;
+        public smallestGap() {
+            Console.WriteLine("Enter number of elements you want in array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            
+            Console.Write("Enter elements in array :- ");
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            if (n == 1)
+            {
+                Console.WriteLine("Array has only one element");
+                return;
+            }
+
+            for(int i=0; i < n; i++)
+            {
+                for (int j = i + 1; j < n - 1; j++)
+                {
+                    if(Math.Abs(arr[i] - arr[j]) < diff)
+                    {
+                        diff=Math.Abs(arr[i] - arr[j]);
+                    }
+                    
+                }
+            }
+            Console.WriteLine($"Minimum gap is :- {diff}");
+        }
+    }
+}
