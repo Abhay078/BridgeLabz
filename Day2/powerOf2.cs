f6a434c1f5033928b3bb0980e86a0f857ee6543c
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-19 13:14:28 +0530

    [Abhay]-Find_table_of_power_of_2

---
+++ Day2/powerOf2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class powerOf2
+    {
+        int n;
+        public powerOf2()
+        {
+            Console.WriteLine("Enter the value of N ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n >= 0 && n < 31)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(Math.Pow(2, i) + " ");
+
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
+
