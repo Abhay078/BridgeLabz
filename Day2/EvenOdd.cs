9cce0baa57ff56aac74e66190643d83565fc35d3
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-19 13:20:44 +0530

    [Abhay]-identify_even_odd

---
+++ Day2/EvenOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class EvenOdd
+    {
+        public EvenOdd() {
+            Console.WriteLine("Please Enter The number");
+            int n=Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Given Number is Even.");
+            }
+            else
+            {
+                Console.WriteLine("Given Number is odd.");
+            }
+        }
+    }
+}
