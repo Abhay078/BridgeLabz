87ca6a9f28dce806ab7de017fbcbe2eb9a8a5f14
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-19 13:21:49 +0530

    [Abhay]-Find_vowel_and_consonent

---
+++ Day2/VowelConsonent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class VowelConsonent
+    {
+        public VowelConsonent() {
+            try
+            {
+                char c = Convert.ToChar(Console.ReadLine());
+                if (c == 'a' || c == 'e' || c == 'o' || c == 'u' || c == 'i' || c == 'A' || c == 'E' || c == 'O' || c == 'U' || c == 'I')
+                {
+                    Console.WriteLine("Entered Character is vowel");
+
+                }
+                else
+                {
+                    Console.WriteLine("Entered Character is consonent");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+        }
+    }
+}
