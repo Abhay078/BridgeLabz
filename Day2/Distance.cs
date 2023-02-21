fd7322514fb98573514cf79db6b3b5ede9b8669f
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-21 23:36:56 +0530

    [Abhay]-Euclidian Distance

---
+++ Day2/Distance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Distance
+    {
+        public Distance() {
+            Console.WriteLine("Enter the value of x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y");
+            int y = Convert.ToInt32(Console.ReadLine());
+            double res=Math.Sqrt(Math.Pow(x,2)+Math.Pow(y,2));
+            Console.WriteLine($"The Euclidian Distance is {res}");
+            Console.ReadLine();
+        }
+    }
+}
