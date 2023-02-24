a9f8f0b0b7bad3d059ea7e302fa997c3b5b34710
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-24 11:42:07 +0530

    [Abhay]-Balance_parenthesis

---
+++ Problems/BalancedParenthesis.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    internal class BalancedParenthesis<T>
+    {
+        
+        public  bool IsBalanced(string expression)
+        {
+            Stack<T> stack =new Stack<T> ();
+            foreach(char c in expression)
+            {
+                if (c.Equals('('))
+                {
+                    stack.Push((T)(object)c);
+                }
+                else if(c == ')')
+                {
+                    if(stack.Count==0 || !stack.Peek().Equals((T)(object)'('))
+                    {
+                        return false;
+                    }
+                    stack.Pop();
+                }
+            }
+            return stack.Count == 0;
+        }
+       
+    }
+}
