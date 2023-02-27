dbc6153661cf256a4fb29018cffd0d466b8d4510
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-27 21:23:44 +0530

    [Abhay]-Food_Inventory_write_to_new json

---
+++ Problems/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class properties
+    {
+        public string name { get; set; }
+        public double weight { get; set; }
+        public double price_per_kg { get; set; }
+        public double total_price { get; set; }
+    }
+    public class Food
+    {
+        public List<properties> rice { get; set; }
+        public List<properties> wheat { get; set; }
+        public List<properties> pulses { get; set; }
+
+
+    }
+}
---
+++ Problems/JsonInventory.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Problems.JsonInventory;
+
+namespace Problems
+{
+    public class JsonInventory
+    {
+        public void ReadJson(string path)
+        {
+            string json=File.ReadAllText(path);
+            dynamic res=Newtonsoft.Json.JsonConvert.DeserializeObject<Food>(json);
+            double trice = 0;
+            double twheat = 0;
+            double tpulse = 0;
+            Food obj=new Food();
+            foreach(var items in res.rice)
+            {
+                trice = items.price_per_kg * items.weight;
+                List<properties> lis=new List<properties>();
+                lis.Add(new properties
+                {
+                    name=items.name,
+                    price_per_kg=items.price_per_kg,
+                    weight=items.weight,
+                    total_price=trice
+
+                });
+                obj.rice = lis;
+                foreach (var item in lis) {
+                    Console.WriteLine(item.name);
+                }
+            }
+
+            foreach (var items in res.wheat)
+            {
+                twheat = items.price_per_kg * items.weight;
+                List<properties> lis = new List<properties>();
+                lis.Add(new properties
+                {
+                    name = items.name,
+                    price_per_kg = items.price_per_kg,
+                    weight = items.weight,
+                    total_price = twheat
+
+                });
+                obj.wheat = lis;
+                
+            }
+            foreach (var items in res.pulses)
+            {
+                tpulse = items.price_per_kg * items.weight;
+                List<properties> lis = new List<properties>();
+                lis.Add(new properties
+                {
+                    name = items.name,
+                    price_per_kg = items.price_per_kg,
+                    weight = items.weight,
+                    total_price = tpulse
+
+                });
+                obj.pulses = lis;
+                
+            }
+            string jsonstring = JsonConvert.SerializeObject(obj);
+            string newfile = "D:\\BridgeLabz_Problems\\Problems\\FoodUpdated.json";
+            File.WriteAllText(newfile, jsonstring);
+        }
+
+    }
+    
+
+
+        
+    
+}
