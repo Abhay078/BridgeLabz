0338a192d9517d121561e0e81afcbf3c68aa44d9
Abhay078 <abhaysrivastava078@gmail.com>
2023-02-27 21:04:18 +0530

    [Abhay]-Validation through regex

---
+++ UserRegistration1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace UserRegistration1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Validation v = new Validation();
+            bool b = true;
+            while (b)
+            {
+                Console.WriteLine("Hello User!");
+                Console.WriteLine("Press 1 for Validating Name\n Press 2 for Validating Email\n Press 3 for Validating Mobile\n Press 4 For validating Password" +
+                    "\n Press 5 for exit");
+                Console.WriteLine("Enter the choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        
+                        Console.WriteLine("Enter the Full name");
+
+                        string name = Console.ReadLine();
+                        bool b1 = v.ValidName(name);
+                        if (b1)
+                        {
+                            Console.WriteLine("Name is valid");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Name is invalid");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Email");
+
+                        string Email = Console.ReadLine();
+                        bool b2 = v.ValidEmail(Email);
+                        if (b2)
+                        {
+                            Console.WriteLine("EMail is valid");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Email is invalid");
+                        }
+
+                        break;
+                        
+                    case 3:
+                        Console.WriteLine("Enter the Mobile");
+                        string Mobile = Console.ReadLine();
+                        bool b3 = v.ValidMobile(Mobile);
+                        if (b3)
+                        {
+                            Console.WriteLine("Mobile is valid");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mobile is invalid");
+                        }
+
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the password");
+                        string password= Console.ReadLine();
+                        bool b4 = v.ValidPass(password);
+                        if (b4)
+                        {
+                            Console.WriteLine("Password is valid");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Password is invalid");
+                        }
+                        break;
+                        case 5:
+                        b = false;
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ UserRegistration1/Validation.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistration1
+{
+    public class Validation
+    {
+        public bool ValidName(string name)
+        {
+            bool flag = false;
+            //string pattern = @"^[A-Z]{1}[a-z]{2,}\s[A-Z]{1}[a-z]{2,}";
+            Regex rx = new Regex(@"^[A-Z]{1}[a-z]{2,}\s[A-Z]{1}[a-z]{2,}");
+            Match match = rx.Match(name);
+            if (match.Success)
+            {
+                flag = true;
+
+            }
+            return flag;
+        }
+        public bool ValidEmail(string email)
+        {
+            bool flag = false;
+            Regex rx = new Regex(@"^[a-z0-9\w]{3,}@([-a-z0-9]+.)+[a-z]{2,5}$");
+            Match match = rx.Match(email);
+            if (match.Success)
+            {
+                flag = true;
+                return flag;
+            }
+
+            return flag;
+            
+        }
+
+        public bool ValidMobile(string mobile)
+        {
+            bool flag = false;
+            Regex rx = new Regex(@"^[0-9]{2}\s[0-9]{10}");
+            Match match = rx.Match(mobile);
+            if(match.Success)
+            {
+                flag = true;
+                return flag;
+
+            }
+            return flag;
+
+           
+        }
+        public bool ValidPass(string pass)
+        {
+            bool flag= false;
+            Regex regex = new Regex(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8}");
+            Match match = regex.Match(pass);
+            if(match.Success)
+            {
+                flag = true;
+                return flag;
+            }
+            return flag;
+        }
+    }
+}
