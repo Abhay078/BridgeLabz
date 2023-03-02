54139707846703257948728a38aa3767aa78e70f
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-02 11:05:34 +0530

    [Abhay]-Ride Repository for storing ride on basis of userid

---
+++ CabInvoiceGenerator/RideRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class RideRepository
+    {
+        Dictionary<string, List<Ride>> pairs = null;
+        public RideRepository() { 
+            this.pairs= new Dictionary<string, List<Ride>>();
+        }
+        
+        public void AddRepo(string userid,Ride[] ride)
+        {
+            bool ridelist=this.pairs.ContainsKey(userid);
+            if(!ridelist)
+            {
+                List<Ride> list = new List<Ride>();
+                list.AddRange(ride);
+                this.pairs.Add(userid, list);
+
+            }
+            
+
+
+        }
+        public Ride[] GetRide(string userId)
+        {
+            return this.pairs[userId].ToArray();
+        }
+    }
+}
