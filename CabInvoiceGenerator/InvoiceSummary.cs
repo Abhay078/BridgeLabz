cd6d7a75eb50c332a78d98afc1987549abc98517
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-02 11:02:25 +0530

    [Abhay]-Invoice summary class with their properties

---
+++ CabInvoiceGenerator/InvoiceSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceSummary
+    {
+        public int totalNumberOfRides { get; set; }
+        public double totalfare { get; set; }
+        public double average { get; set; }
+        public void CalculateAverage()
+        {
+            average = totalfare / totalNumberOfRides;
+
+        }
+
+    }
+}
